3ffff531063b8fffa72a2d36a9320ffcb2a27c16
kirishree <115062611+kirishree@users.noreply.github.com>
2023-06-02 17:24:23 +0530

    Create pci_details.py

---
+++ pci_details.py
@@ -0,0 +1,21 @@
+import os
+import subprocess
+def pci_details():
+  pci_out = subprocess.check_output(["lspci"]).decode().split("\n")
+  data = []
+  for line in pci_out:
+    if "Ethernet controller" in line:
+      pci_info = "0000:"+line.split()[0]
+      lsh_out = subprocess.check_output(["lshw", "-c", "network", "-businfo"])
+      lsh_out = lsh_out.decode().split("\n")
+      lsh_out = lsh_out[2:]
+      for line in lsh_out:
+        li = line.split()
+        lii = li[0].split("@")
+        pci_addr = lii[1]
+        if pci_info == pci_addr:
+           colect = {"pci_address":pci_info", "interface_name":li[1]}
+     
+    data.append(colect)
+  return data
+print(pci_details())
