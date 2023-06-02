54b087c8c0fe07d3575c657e63185eb3df98e9a4
kirishree <115062611+kirishree@users.noreply.github.com>
2023-06-02 17:51:25 +0530

    Update pci_details.py

--- pci_details.py
+++ pci_details.py
@@ -8,14 +8,15 @@ def pci_details():
       pci_info = "0000:"+line.split()[0]
       lsh_out = subprocess.check_output(["lshw", "-c", "network", "-businfo"])
       lsh_out = lsh_out.decode().split("\n")
-      lsh_out = lsh_out[2:]
+      lsh_out = lsh_out[2:-1]
       for line in lsh_out:
         li = line.split()
-        lii = li[0].split("@")
-        pci_addr = lii[1]
+        pci_addr = li[0].split("@")[1]
         if pci_info == pci_addr:
-           colect = {"pci_address":pci_info", "interface_name":li[1]}
-     
-    data.append(colect)
+           colect = {
+             "pci_address":pci_info", 
+             "interface_name":li[1]
+           }
+           data.append(colect)
   return data
 print(pci_details())
